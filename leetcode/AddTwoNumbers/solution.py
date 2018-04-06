b571a5174ffda519d757d4411074f2eb4d78428d
sunny809 <sunny809@163.com>
2018-04-06 13:59:45 +0800

    add add two numbers solutions

--- leetcode/AddTwoNumbers/solution.py
+++ leetcode/AddTwoNumbers/solution.py
@@ -4,6 +4,15 @@
 #         self.val = x
 #         self.next = None
 
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+
 class Solution:
     def addTwoNumbers(self, l1, l2):
         """
@@ -11,3 +20,30 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
+        list1 = self.get_list_from_list_node(l1)
+        list2 = self.get_list_from_list_node(l2)
+
+        return self.getNumberFromList(list1) + self.getNumberFromList(list2);
+
+    def getNumberFromList(self, input_list):
+        result_num = 0
+
+        for num_index in range(0, len(input_list)):
+            num_index = len(input_list) - (num_index + 1)
+            num = input_list[num_index]
+            result_num = result_num + (num * (10 ** num_index))
+
+        return result_num
+
+    def get_list_from_list_node(self, ln):
+        result = []
+
+        while ln is not None:
+            result.append(ln.val)
+            ln = ln.next
+
+        result.reverse()
+        return result
+
+    def get_list_from_number(self, input_num):
+
---
+++ leetcode/AddTwoNumbers/solution_test.py
@@ -0,0 +1,93 @@
+import unittest;
+from leetcode.AddTwoNumbers.solution import Solution
+from leetcode.AddTwoNumbers.solution import ListNode
+
+# from https://leetcode-cn.com/problems/add-two-numbers/description/
+
+class SolutionTest(unittest.TestCase):
+
+    def genListNode(self, input_array):
+
+        before_node = None
+        first_node = None
+
+        for index in range(0, len(input_array)):
+            print(input_array[index])
+            current_node = ListNode(input_array[index])
+
+            if before_node is None:
+                first_node = current_node
+            else:
+                before_node.next = current_node
+            before_node = current_node
+
+        return first_node
+
+
+    def test_basic(self):
+        l1 = [2,3,4]
+        result_list_node = self.genListNode(l1)
+        print (result_list_node)
+
+        while result_list_node is not None:
+            print(result_list_node.val)
+            result_list_node = result_list_node.next
+
+
+
+
+    def test_simple(self):
+        ln1 = self.genListNode([2,4,3])
+        # 342
+        ln2 = self.genListNode([5,6,4])
+        # 465
+        solution = Solution()
+        result = solution.addTwoNumbers(ln1,ln2)
+        # 342 + 564 = 807
+        self.assertEqual(807,result)
+
+    def test_1(self):
+        ln1 = self.genListNode( [1,2,3])
+        # 321
+        ln2 = self.genListNode([3,4,5])
+        # 543
+        solution = Solution()
+        result = solution.addTwoNumbers(ln1,ln2)
+        # 321 + 543 = 864
+        self.assertEqual(864,result)
+
+    def test_2(self):
+        l1 = [7,0,0]
+        # 321
+        l2 = [8,0,0]
+        # 543
+        solution = Solution()
+        result = solution.addTwoNumbers(self.genListNode(l1),self.genListNode(l2))
+        # 321 + 543 = 864
+        self.assertEqual(15,result)
+
+    # def test_1(self):
+    #     self.data = [15,11,7,2]
+    #     solution = Solution()
+    #     result = solution.twoSum(self.data, 9)
+    #     self.assertEqual([2,3],result)
+
+    # def test_2(self):
+    #     self.data = [4,4]
+    #     solution = Solution()
+    #     result = solution.twoSum(self.data, 8)
+    #     self.assertEqual([0,1],result)
+    #
+    # def test_3(self):
+    #     self.data = [0,4,3,0]
+    #     # self.data.reverse()
+    #     solution = Solution()
+    #     result = solution.twoSum(self.data, 0)
+    #     self.assertEqual([0,3],result)
+    #
+    # def test_4(self):
+    #     self.data = [-3,4,3,90]
+    #     # self.data.reverse()
+    #     solution = Solution()
+    #     result = solution.twoSum(self.data, 0)
+    #     self.assertEqual([0,2],result)
